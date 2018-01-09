153154361ac205d64f1705f3429a801b5ed64699
nspp99b <chromepanthers@gmail.com>
2018-01-09 11:47:16 -0500

    defined all function expressions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (arr) {
+  return arr.slice(0,2)
+}
+
+const returnLastTwoDrivers = function (arr) {
+  return arr.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (mult) {
+  return (function (num) { return mult * num })
+}
+
+const fareDoubler = function (fare) {
+  return createFareMultiplier(2)(fare)
+}
+
+const fareTripler = function (fare) {
+  return createFareMultiplier(3)(fare)
+}
+
+const selectDifferentDrivers = function (arr, func) {
+  return func(arr)
+}
